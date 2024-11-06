d95bb98cb3577e7ba298701a83466ceb0db688c0
ing-santi-garcia <ing.santiago.garcia10@gmail.com>
2024-11-06 17:58:28 -0500

    Creando deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ing-santi-garcia.github.io',
+  base: 'ing-santi-garcia',
   integrations: [tailwind()]
 });
