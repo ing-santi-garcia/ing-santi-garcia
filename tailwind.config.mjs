0cf82778f97c2c660694cceaf4545ee5a1cf5c36
ing-santi-garcia <ing.santiago.garcia10@gmail.com>
2024-11-06 18:29:51 -0500

    Deploy update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: 'none',
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
